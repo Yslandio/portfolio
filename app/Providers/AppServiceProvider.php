b37bb036dc96f95352bcfec9c2058b89d4460f03
Yslandio <yslandiosouza2010@gmail.com>
2022-07-13 15:58:25 -0300

    teste

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        $this->app->bind('path.public', function() {
+            // return your own correct path.
+            return realpath(base_path('../'));
+       });
     }
 }
